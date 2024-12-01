f199b058270dcba6ce2e65d28363f6601e4f9ccd
ccgauche <gaucheron.laurent@gmail.com>
2024-12-01 23:07:51 +0100

    Add a way to force the browser

--- src/main.rs
+++ src/main.rs
@@ -94,7 +94,10 @@ async fn main() {
                 return;
             }
             "--with-auto-cookies" => {
-                if let Some(cookies) = cookies() {
+                std::fs::write(get_log_file_path(), "# YTerMusic log file\n\n").unwrap();
+                init().expect("Failed to initialize logger");
+                let param = std::env::args().nth(2);
+                if let Some(cookies) = cookies(param) {
                     let mut cookies_guard = COOKIES.write().unwrap();
                     info!("Cookies: {cookies}");
                     *cookies_guard = Some(cookies);
@@ -114,6 +117,9 @@ async fn main() {
                 return;
             }
         }
+    } else {
+        std::fs::write(get_log_file_path(), "# YTerMusic log file\n\n").unwrap();
+        init().expect("Failed to initialize logger");
     }
     panic::set_hook(Box::new(|e| {
         println!("{e}");
@@ -131,13 +137,41 @@ async fn main() {
     };
 }
 
-fn cookies() -> Option<String> {
+fn cookies(specific_browser: Option<String>) -> Option<String> {
+    let loaded = match specific_browser {
+        Some(browser) => match browser.as_str() {
+            "all" => rookie::load,
+            "firefox" => rookie::firefox,
+            "chrome" => rookie::chrome,
+            "edge" => rookie::edge,
+            "opera" => rookie::opera,
+            "brave" => rookie::brave,
+            "vivaldi" => rookie::vivaldi,
+            "chromium" => rookie::chromium,
+            #[cfg(target_os = "macos")]
+            "safari" => rookie::safari,
+            "arc" => rookie::arc,
+            "librewolf" => rookie::librewolf,
+            "opera-gx" | "opera_gx" => rookie::opera_gx,
+            #[cfg(target_os = "windows")]
+            "internet_explorer" | "internet-explorer" | "ie" => rookie::internet_explorer,
+            #[cfg(target_os = "windows")]
+            "octo_browser" | "octo-browser" => rookie::octo_browser,
+            _ => {
+                println!("Unknown browser `{browser}`");
+                error!("Unknown browser `{browser}`");
+                return None;
+            }
+        },
+        None => rookie::load,
+    }(Some(vec!["youtube.com".to_string()]))
+    .unwrap();
     let mut cookies = Vec::new();
     let current_timestamp = std::time::SystemTime::now()
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs();
-    for cookie in rookie::load(Some(vec!["youtube.com".to_string()])).unwrap() {
+    for cookie in loaded {
         if cookie.domain != ".youtube.com" && cookie.domain != "music.youtube.com" {
             continue;
         }
@@ -185,8 +219,6 @@ fn get_header_file() -> Result<(String, PathBuf), (std::io::Error, PathBuf)> {
     std::fs::read_to_string(&fp).map_or_else(|e| Err((e, fp.clone())), |e| Ok((e, fp.clone())))
 }
 async fn app_start() {
-    std::fs::write(get_log_file_path(), "# YTerMusic log file\n\n").unwrap();
-    init().expect("Failed to initialize logger");
     STARTUP_TIME.log("Init");
 
     std::fs::create_dir_all(CACHE_DIR.join("downloads")).unwrap();
